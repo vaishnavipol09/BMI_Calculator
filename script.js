b298cb0f444cb3283b947e1e522e209409e5b875
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-03-25 17:25:11 +0530

    js project completed

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("form")
+//0const height = parseInt(document.querySelector("#height").value) - this usecase will give you empty
+
+form.addEventListener("submit", function(e)  {
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const results = document.querySelector("#results")
+
+    if(height === " " || height < 0 || isNaN(height)){
+        results.innerHTML =  `Please enter a valid height ${height}`
+    } else if(weight === " " || weight < 0 || isNaN(weight)){
+        results.innerHTML =  `Please enter a valid weight ${weight}`
+    } else{
+        const bmi = (weight / ((height*height) / 10000)).toFixed(2) // formula
+        // show the results
+        results.innerHTML =  `Your BMI is ${bmi}`
+    }
+})
